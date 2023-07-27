1bc3a37feee86d91ca287fd781b90927ea38adbe
Fahmi <junanda06@gmail.com>
2023-07-27 21:58:57 +0700

    a

---
+++ lib/bindings/register_binding.dart
@@ -0,0 +1,9 @@
+import 'package:donor_darah/controllers/registercontroller.dart';
+import 'package:get/get.dart';
+
+class RegisterBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<RegisterController>(() => RegisterController());
+  }
+}
